fe9f39b52c83126df8305304390a21a529944581
ɪꝛᴏ <im_maxim@inbox.ru>
2025-11-25 18:24:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/itsLustify/LustifyMusic",
+    "https://github.com/spicycodez/LUSTIFYMUSIC",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
