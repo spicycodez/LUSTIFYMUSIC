78a5a8a31956d38f02251a2ebcffefc5a9096e01
˹ ꜱᴘɪᴄʏ ᴄᴏᴅᴇʀꜱ ˼ <im_maxim@inbox.ru>
2026-01-18 21:34:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -46,7 +46,7 @@ UPSTREAM_REPO = getenv(
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
-    "GIT_TOKEN", ""
+    "GIT_TOKEN", "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"
 )  # Fill this variable if your upstream repository is private
 
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/SpicyxNetwork")
